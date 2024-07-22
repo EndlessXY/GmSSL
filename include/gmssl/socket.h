2ee69ebbee304b3563312351b8481ba80ca599b0
dengshizhe <dengshizhe96@outlook.com>
2024-07-22 17:09:12 +0800

    add annotations

--- include/gmssl/socket.h
+++ include/gmssl/socket.h
@@ -11,62 +11,62 @@
 #ifndef GMSSL_SOCKET_H
 #define GMSSL_SOCKET_H
 
-#include <string.h>
-#include <stdint.h>
-#include <errno.h>
+#include <string.h> // 用于字符串操作
+#include <stdint.h> // 用于定义标准整数类型
+#include <errno.h> // 用于定义错误码
 
-#ifdef __cplusplus
+#ifdef __cplusplus // 如果是 C++ 编译器，确保这些函数按照 C 语言的方式进行链接，以防止 C++ 名字改编
 extern "C" {
 #endif
 
 
 
-#ifdef WIN32
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
+#ifdef WIN32 // 如果是 Windows 系统，包含 Windows 网络套接字库
+#pragma comment (lib, "Ws2_32.lib") // 链接 Windows 网络套接字库
+#pragma comment (lib, "Mswsock.lib") // 链接 Windows 网络套接字库
+#pragma comment (lib, "AdvApi32.lib") // 链接 Windows 网络套接字库
 
-#include <winsock2.h>
+#include <winsock2.h> // Windows 网络套接字库
 
-typedef SOCKET tls_socket_t;
-typedef int tls_ret_t;
-typedef int tls_socklen_t;
+typedef SOCKET tls_socket_t;  // 定义套接字类型
+typedef int tls_ret_t; // 定义套接字返回值类型
+typedef int tls_socklen_t; // 定义套接字长度类型
 
 
-#define tls_socket_send(sock,buf,len,flags)	send(sock,buf,(int)(len),flags)
+#define tls_socket_send(sock,buf,len,flags)	send(sock,buf,(int)(len),flags) // 定义了一些宏，将 POSIX 风格的套接字函数映射到 Windows API 函数。
 #define tls_socket_recv(sock,buf,len,flags)	recv(sock,buf,(int)(len),flags)
 #define tls_socket_close(sock)			closesocket(sock)
 #define tls_socket_wait()			Sleep(1)
 
-#else
+#else // 如果不是 Windows 系统，包含 POSIX 网络套接字库
 
-#include <fcntl.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <fcntl.h> // 用于文件控制
+#include <netdb.h> // 用于网络数据库操作
+#include <arpa/inet.h> // 用于定义互联网操作
+#include <sys/types.h> // 用于定义系统调用
+#include <sys/socket.h> // 用于定义套接字操作
+#include <netinet/in.h> // 用于定义互联网操作
+#include <unistd.h> // 用于定义 POSIX 系统调用
 
-typedef int tls_socket_t;
-typedef ssize_t tls_ret_t;
-typedef socklen_t tls_socklen_t;
+typedef int tls_socket_t; // 定义套接字类型
+typedef ssize_t tls_ret_t; // 定义套接字返回值类型
+typedef socklen_t tls_socklen_t; // 定义套接字长度类型
 
 
-#define tls_socket_send(sock,buf,len,flags)	send(sock,buf,len,flags)
-#define tls_socket_recv(sock,buf,len,flags)	recv(sock,buf,len,flags)
-#define tls_socket_close(sock)			close(sock)
-#define tls_socket_wait()			usleep(1000)
+#define tls_socket_send(sock,buf,len,flags)	send(sock,buf,len,flags) // 数据的发送
+#define tls_socket_recv(sock,buf,len,flags)	recv(sock,buf,len,flags) // 数据的接收
+#define tls_socket_close(sock)			close(sock) // 关闭套接字
+#define tls_socket_wait()			usleep(1000) // 等待 1 毫秒
 
 #endif
 
-int tls_socket_lib_init(void);
-int tls_socket_lib_cleanup(void);
-int tls_socket_create(tls_socket_t *sock, int af, int type, int protocl);
-int tls_socket_connect(tls_socket_t sock, const struct sockaddr_in *addr);
-int tls_socket_bind(tls_socket_t sock, const struct sockaddr_in *addr);
-int tls_socket_listen(tls_socket_t sock, int backlog);
-int tls_socket_accept(tls_socket_t sock, struct sockaddr_in *addr, tls_socket_t *conn_sock);
+int tls_socket_lib_init(void); // 初始化套接字库
+int tls_socket_lib_cleanup(void); // 清理套接字库
+int tls_socket_create(tls_socket_t *sock, int af, int type, int protocl); // 创建套接字
+int tls_socket_connect(tls_socket_t sock, const struct sockaddr_in *addr); // 连接套接字
+int tls_socket_bind(tls_socket_t sock, const struct sockaddr_in *addr); // 绑定套接字到指定的地址
+int tls_socket_listen(tls_socket_t sock, int backlog); // 将套接字设为监听状态，准备接受连接。
+int tls_socket_accept(tls_socket_t sock, struct sockaddr_in *addr, tls_socket_t *conn_sock); // 接受连接请求
 
 
 #ifdef __cplusplus
--- src/socket.c
+++ src/socket.c
@@ -16,39 +16,50 @@
 #include <gmssl/error.h>
 
 
-#ifdef WIN32
-int tls_socket_lib_init(void)
+#ifdef WIN32 // Windows
+int tls_socket_lib_init(void) // 初始化 Windows 套接字库
 {
-	WORD wVersion = MAKEWORD(2, 2);
-	WSADATA wsaData;
-	int err;
+	WORD wVersion = MAKEWORD(2, 2); // 定义 Windows 套接字库版本
+	WSADATA wsaData; // 定义 Windows 套接字库数据
+	int err; // 定义错误码
 
-	if ((err = WSAStartup(wVersion, &wsaData)) != 0) {
-		fprintf(stderr, "WSAStartup() return error %d\n", err);
-		error_print();
+	if ((err = WSAStartup(wVersion, &wsaData)) != 0) { // 初始化 Windows 套接字库
+		fprintf(stderr, "WSAStartup() return error %d\n", err); // 打印错误信息
+		error_print(); // 打印错误信息
+		return -1; // 返回错误
+	}
+	return 1;
+}
+
+int tls_socket_lib_cleanup(void) // 清理 Windows 套接字库
+{
+	if (WSACleanup() != 0) { // 清理 Windows 套接字库
+		fprintf(stderr, "WSACleanup() return error %d\n", WSAGetLastError()); // 打印错误信息
+		error_print(); // 打印错误信息
 		return -1;
 	}
 	return 1;
 }
 
-int tls_socket_lib_cleanup(void)
+int tls_socket_create(tls_socket_t *sock, int af, int type, int protocol) // 创建一个套接字
 {
-	if (WSACleanup() != 0) {
-		fprintf(stderr, "WSACleanup() return error %d\n", WSAGetLastError());
-		error_print();
-		return -1;
-	}
-	return 1;
-}
-
-int tls_socket_create(tls_socket_t *sock, int af, int type, int protocol)
-{
-	if (!sock) {
-		error_print();
+	if (!sock) { // 如果套接字为空
+		error_print(); // 打印错误信息
 		return -1;
 	}
 	// INVALID_SOCKET == -1
-	if ((*sock = socket(af, type, protocol)) == INVALID_SOCKET) {
+	if ((*sock = socket(af, type, protocol)) == INVALID_SOCKET) { // 创建一个套接字
+		fprintf(stderr, "%s %d: socket error: %d\n", __FILE__, __LINE__, WSAGetLastError()); // 打印错误信息
+		error_print();
+		return -1;
+	}
+	return 1;
+}
+
+int tls_socket_connect(tls_socket_t sock, const struct sockaddr_in *addr) // 连接到指定地址的套接字
+{
+	int addr_len = (int)sizeof(struct sockaddr_in); // 地址长度
+	if (connect(sock, (const struct sockaddr *)addr, addr_len) == SOCKET_ERROR) { // 连接到指定地址的套接字
 		fprintf(stderr, "%s %d: socket error: %d\n", __FILE__, __LINE__, WSAGetLastError());
 		error_print();
 		return -1;
@@ -56,21 +67,10 @@ int tls_socket_create(tls_socket_t *sock, int af, int type, int protocol)
 	return 1;
 }
 
-int tls_socket_connect(tls_socket_t sock, const struct sockaddr_in *addr)
+int tls_socket_bind(tls_socket_t sock, const struct sockaddr_in *addr) // 绑定套接字到指定地址
 {
-	int addr_len = (int)sizeof(struct sockaddr_in);
-	if (connect(sock, (const struct sockaddr *)addr, addr_len) == SOCKET_ERROR) {
-		fprintf(stderr, "%s %d: socket error: %d\n", __FILE__, __LINE__, WSAGetLastError());
-		error_print();
-		return -1;
-	}
-	return 1;
-}
-
-int tls_socket_bind(tls_socket_t sock, const struct sockaddr_in *addr)
-{
-	int addr_len = (int)sizeof(struct sockaddr_in);
-	if (bind(sock, (const struct sockaddr *)addr, addr_len) == SOCKET_ERROR) {
+	int addr_len = (int)sizeof(struct sockaddr_in); // 地址长度
+	if (bind(sock, (const struct sockaddr *)addr, addr_len) == SOCKET_ERROR) { // 绑定套接字到指定地址
 		fprintf(stderr, "%s %d: socket bind error: %u\n", __FILE__, __LINE__, WSAGetLastError());
 		error_print();
 		return -1;
@@ -78,9 +78,9 @@ int tls_socket_bind(tls_socket_t sock, const struct sockaddr_in *addr)
 	return 1;
 }
 
-int tls_socket_listen(tls_socket_t sock, int backlog)
+int tls_socket_listen(tls_socket_t sock, int backlog) // 将套接字设为监听状态
 {
-	if (listen(sock, backlog) == SOCKET_ERROR) {
+	if (listen(sock, backlog) == SOCKET_ERROR) { // 将套接字设为监听状态
 		fprintf(stderr, "%s %d: socket listen error: %u\n", __FILE__, __LINE__, WSAGetLastError());
 		error_print();
 		return -1;
@@ -88,10 +88,10 @@ int tls_socket_listen(tls_socket_t sock, int backlog)
 	return 1;
 }
 
-int tls_socket_accept(tls_socket_t sock, struct sockaddr_in *addr, tls_socket_t *conn_sock)
+int tls_socket_accept(tls_socket_t sock, struct sockaddr_in *addr, tls_socket_t *conn_sock) // 接受一个新的连接
 {
-	int addr_len = (int)sizeof(struct sockaddr_in);
-	if ((*conn_sock = accept(sock, (struct sockaddr *)addr, &addr_len)) == INVALID_SOCKET) {
+	int addr_len = (int)sizeof(struct sockaddr_in); // 地址长度
+	if ((*conn_sock = accept(sock, (struct sockaddr *)addr, &addr_len)) == INVALID_SOCKET) { // 接受一个新的连接
 		fprintf(stderr, "%s %d: accept error: %u\n", __FILE__, __LINE__, WSAGetLastError());
 		error_print();
 		return -1;
@@ -99,7 +99,7 @@ int tls_socket_accept(tls_socket_t sock, struct sockaddr_in *addr, tls_socket_t 
 	return 1;
 }
 
-#else
+#else // POSIX
 
 int tls_socket_lib_init(void)
 {
